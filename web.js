7648992ad22aab20828d8219b7523b58ad8776c3
당ìATLANTIS <atlantisro2021@naver.com>
2023-10-11 21:11:46 +0900

    message

--- web.js
+++ web.js
@@ -19,7 +19,7 @@ require('dotenv').config()
 const connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'nice2021!',
+    password : '',
     database : 'test'
 });
 
